ee04847ec26ffa31e79a25e396007f8c754a3ea9
BTslayer761 <brendantey761@gmail.com>
2025-10-05 14:39:26 +0800

    Add MarkCommand

--- src/main/java/astra/command/MarkCommand.java
+++ src/main/java/astra/command/MarkCommand.java
@@ -1,6 +1,8 @@
 package astra.command;
 
 import astra.activity.ActivityList;
+import astra.activity.Activity;
+import astra.activity.Task;
 import astra.data.Notebook;
 import astra.ui.Ui;
 
@@ -8,8 +10,26 @@ public class MarkCommand implements Command {
     private int index;
     //private ActivityList activities;
 
+    /**
+     * Only works if the Activity is an instanceof Task.
+     */
     @Override
     public boolean execute(ActivityList activities, Ui ui, Notebook notebook) {
+        try {
+            Activity currActivity = activities.getActivity(index);
+            if (!(currActivity instanceof Task)) {
+                ui.showError("Activity at index " + index + " is not a Task");
+                return false;
+            }
+            if (((Task) currActivity).getIsComplete()) {
+                ui.showError("Activity at index " + index + " has already completed");
+                return false;
+            }
+            ((Task) currActivity).setIsComplete();
+            ui.showMessage("Successfully marked task at index " + index);
+        } catch (IndexOutOfBoundsException e) {
+            ui.showError("index " + index + " is out of bounds.");
+        }
         return false;
     }
 }
