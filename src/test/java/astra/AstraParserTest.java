5428b9ab82493ac06b373d88800a29200c8f1577
BTslayer761 <brendantey761@gmail.com>
2025-10-12 21:35:29 +0800

    Add ByteArrayOutputStream and PrintStream to check error messages for invalid input of CompleteCommand

--- src/test/java/astra/AstraParserTest.java
+++ src/test/java/astra/AstraParserTest.java
@@ -13,23 +13,48 @@ import astra.parser.Parser;
 import astra.ui.Ui;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 
 public class AstraParserTest {
 
-    /** User interface handler for displaying messages and interactions */
+    /**
+     * User interface handler for displaying messages and interactions
+     */
     private Ui ui;
 
-    /** Storage handler for saving and loading of activithelpies */
+    /**
+     * Storage handler for saving and loading of activithelpies
+     */
     private Notebook notebook;
 
-    /** Stores user activities during runtime */
+    /**
+     * Stores user activities during runtime
+     */
     private ActivityList activities;
+
+    /**
+     * capture System.out
+     */
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    /**
+     * store real console output
+     */
+    private final PrintStream originalOut = System.out;
 
     private void setup() {
         this.ui = new Ui();
         this.activities = new ActivityList();
         this.notebook = new Notebook("test.txt");
+        System.setOut(new PrintStream(outContent));
     }
+
+    void tearDown() {
+        System.setOut(originalOut); // restore normal console output
+    }
+
 
     @Test
     public void testAddTask_validInput_expectSuccess() {
@@ -41,6 +66,7 @@ public class AstraParserTest {
         } catch (InputException e) {
             System.out.println(e.getMessage());
         }
+        tearDown();
     }
 
     @Test
@@ -53,6 +79,7 @@ public class AstraParserTest {
         } catch (InputException e) {
             System.out.println(e.getMessage());
         }
+        tearDown();
     }
 
     @Test
@@ -65,5 +92,6 @@ public class AstraParserTest {
         } catch (InputException e) {
             System.out.println(e.getMessage());
         }
+        tearDown();
     }
 }
